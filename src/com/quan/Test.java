37b8cb254f1f432a1d684fd81565c6fc5b062437
quanhc <qhc8088@163.com>
2018-07-31 15:48:26 +0800

    2222

--- src/com/quan/Test.java
+++ src/com/quan/Test.java
@@ -2,6 +2,6 @@ package com.quan;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!!");
+        System.out.println("Hello World!!!@#$!");
     }
 }
